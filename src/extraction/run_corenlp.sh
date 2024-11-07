bc5e04d87274ad7eb63f8ac7361721a73dceabc7
zavavan <zavavan@yahoo.it>
2024-11-07 11:09:23 +0100

    ''

--- src/extraction/run_corenlp.sh
+++ src/extraction/run_corenlp.sh
@@ -1,10 +1,4 @@
-
-wget https://downloads.cs.stanford.edu/nlp/software/stanford-corenlp-latest.zip
-unzip stanford-corenlp-latest.zip
-rm stanford-corenlp-latest.zip
-
-cd stanford-corenlp-4.4.0
-java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9050 -timeout 15000 -threads 4 &
+java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9050 -timeout 15000 -threads 4 '&'
 corenlp_process_pid=$!
 cd ..
 echo $corenlp_process_pid
