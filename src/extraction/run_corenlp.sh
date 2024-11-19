5e50320b7b9304c26a43712d8f4ef172c85540a5
zavavan <zavavan@yahoo.it>
2024-11-19 11:33:13 +0100

    ''

--- src/extraction/run_corenlp.sh
+++ src/extraction/run_corenlp.sh
@@ -1,6 +1,8 @@
+cd stanford-corenlp-4.5.7
+java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9050 -timeout 15000 -threads 4 &
 corenlp_process_pid=$!
 cd ..
 echo $corenlp_process_pid
-python corenlp_extractor.py
+python corenlp_extractor.py 4
 kill -9 $corenlp_process_pid
 
